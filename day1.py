3fd88704939f006bcb3deaa18cb857d19ede7685
jayeshkkk <98467816+jayeshkkk@users.noreply.github.com>
2026-09-12 23:11:47 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,15 @@
+# its a object oreinted programming language .
+#python is dynamically typed .
+#Guido Von rossem in 1991
+#its a interprited high leve;l programing language
+
+
+
+
+#features -
+#platform independent
+#open source programming language 
+#provides a big library support
+#easy to integrate with other programming language
+print ("hello world")
+print("nam")
---
+++ day3.py
@@ -0,0 +1,7 @@
+#modules 
+#MODULES ARE OF TWO TYPES -
+#built in modules 
+#user defined modules(external modules)
+
+#*******REPL-read evaluate print loop **********
+
