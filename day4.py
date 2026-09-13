f51a40af73fa73fef8ec86c6974e731563956f2f
Jayesh Khandelwal <98467816+jayeshkkk@users.noreply.github.com>
2026-09-13 22:07:17 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,6 @@
+print("hello world")#THIS IS A FUNCTION USED FOR WRITING YOU HAVE
+#TO PROVIDE A VALID
+'''function in the brackets normal english words arnt a valid 
+unction yoyu have to provide a string for them a example for
+valid function is integer and numbers ......'''
+print (5,"fine shyt")
