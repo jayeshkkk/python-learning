aa05841a14f69c7088754aaba3338fd6734eabcd
Jayesh Khandelwal <98467816+jayeshkkk@users.noreply.github.com>
2026-09-13 23:00:14 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,31 @@
+#ESCAPE SEQUENCE - IN PYTHON US \N
+#this is a comment ..........
+# jncsdkjcnds
+# dvnskdj
+# cdkjcsd
+# ksjdc n
+'''hello
+this is a 
+multiline comment
+just add triple single coat at the starting 
+and the end of the comment and it will be treated as a comment'''
+
+
+#*****************************************************************************
+'''IN STRINGS IF I HAVE TO USE "" I HAVE TO USE IT /"JNDJN"/ WAY '''
+
+
+
+
+
+#*****************************#
+print("hello" ,6,7,89,36, sep="*", end =" 0000")
+'''
+in this the sep represent the seperator which is 
+used to separate the values in the print function by 
+default it is a space but we can change it to any character
+we want
+and the end represent what you want to print at the end of the print 
+function by default it is a new line but we can change it to any 
+character we want
+'''
