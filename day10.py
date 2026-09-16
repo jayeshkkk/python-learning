98efdf73351b16fbb088e5bef5c2d74a48f2a389
Jayesh Khandelwal <98467816+jayeshkkk@users.noreply.github.com>
2026-09-16 21:23:23 +0530

    Add files via upload

---
+++ day10.py
@@ -0,0 +1,12 @@
+#this lecture is about user input in python
+'''syntex of input function is input("message") and 
+it will take the input from user and store it in a variable'''
+a=input("enter your name")
+print("hello",a)
+
+#by default input function takes the input in string format .
+b=int(input("enter your age"))
+c=input("enter a number")
+print(type(c))
+print("your age is",b)
+print(type(b))
