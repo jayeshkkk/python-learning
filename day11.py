7f34692fb74cb9a6510a8f41f6e4bc3118fc732e
Jayesh Khandelwal <98467816+jayeshkkk@users.noreply.github.com>
2026-09-17 22:00:21 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,20 @@
+#THIS LECTURE IS ABOUT STRINGS IN PYTHON
+'''if i have to use "" in a string i have to use it /"jndjn"/ way
+or i can use single quotes in the string and it will work fine
+'''
+
+#******************MULTILINE STRING*************************#
+'''we can use triple single quotes or triple double quotes
+ to write a multiline string'''
+
+
+
+'''string can be called numrically like list also
+'''
+#EXAMPLE-
+a="hello this is a string"
+print(a[4])
+#print(a[22]) #throws a index error
+#INDEX ERROR -  the part(index ) oyu are triying to reach is not present in the string
+for char in a:
+    print(char)
